1137836274a7321fec90d3bca9c44298104b880f
Hawkzilla <83360112+Hawkzilla@users.noreply.github.com>
2026-02-25 14:05:52 +0800

    Update PUT response codes to include 200

--- provider_client.go
+++ provider_client.go
@@ -641,7 +641,7 @@ func defaultOkCodes(method string) []int {
 	case "POST":
 		return []int{201, 202}
 	case "PUT":
-		return []int{201, 202}
+		return []int{200, 201, 202}
 	case "PATCH":
 		return []int{200, 202, 204}
 	case "DELETE":
